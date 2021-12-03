98d6040d7816449173cba93afafd918fe075d658
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2021-12-03 15:21:23 -0500

    Async added!

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,11 +1,15 @@
+import asyncio
 import os
 import sys
+
 sys.path.insert(0, os.getcwd())
 from scripts.load_sql import LoadSQL
 from webxplore import WebScraper
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from scripts.models import Article, create_table
+from asgiref.sync import sync_to_async
+
 
 class CreateDatabase:
     def __init__(self):
@@ -17,20 +21,29 @@ class CreateDatabase:
         self.session = Session()
         create_table(engine=engine)
 
-    def populate_db(self, sample_size=1000):
+    async def populate_db(self, sample_size=1000):
         data_provider = LoadSQL()
         articles = data_provider.get_dataset(sample_size=sample_size)
-        for i, record in enumerate(articles.to_dict(orient='records')):
-            try:
-                text = WebScraper.ScrapeWebsite(record['url'], timeout=5).text_content
-                if not i%25:
-                    print(i)
-                article = Article(source=record['source'], text=text, label=int(record['label']))
-                self.session.add(article)
-            except Exception as e:
-                print(f'Error Encountered: {e}')
-                continue
+        list_tasks = []
+        for record in articles.to_dict(orient='records'):
+            list_tasks.append(self.return_article_content(record))
+
+        list_output = await asyncio.gather(*list_tasks)
+
+        for record, output in list_output:
+            article = Article(source=record['source'], text=output, label=int(record['label']))
+            self.session.add(article)
+
         self.session.commit()
+
+    async def return_article_content(self, record):
+        try:
+            return_t = await sync_to_async(WebScraper.ScrapeWebsite)(record['url'], timeout=5)
+            return_t = return_t.text_content
+        except Exception as e:
+            print(f'Error Encountered: {e}')
+            return ''
+        return record, return_t
 
     def close_db_conn(self):
         self.session.close()
@@ -38,5 +51,5 @@ class CreateDatabase:
 
 # For testing purposes
 # db = CreateDatabase()
-# db.populate_db(sample_size=10_000)
+# asyncio.run(db.populate_db(sample_size=10000))
 # db.close_db_conn()
